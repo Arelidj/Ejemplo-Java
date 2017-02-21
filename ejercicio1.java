6182e22f2a612658f1176d4ee5cf9b3c4a2d2cbd
AreliDJ <162t0302@itsm.edu.mx>
2017-02-20 21:14:15 -0600

    Create ejercicio1.java

---
+++ ejercicio1.java
@@ -0,0 +1,13 @@
+public class ejercicio1 {
+	public static void main(String []args ){
+		rectangulo();
+	}
+	public static void rectangulo(){
+		int base=3;
+		int altura=6;
+		int area= base*altura;
+		System.out.println("El area del rectangulo es = "+ area);
+		
+	}
+
+}
