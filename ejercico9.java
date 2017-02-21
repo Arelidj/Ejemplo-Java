660f6786448beee9f40e584c87853634a50afe24
AreliDJ <162t0302@itsm.edu.mx>
2017-02-20 21:19:14 -0600

    Create ejercico9.java

---
+++ ejercico9.java
@@ -0,0 +1,13 @@
+public class ejercico9 {
+	public static void main(String[]args){
+		nummayor(3,5);
+	}
+	public static void nummayor(int num1, int num2){
+		if (num1>num2){
+			System.out.println("El numero mayor es "+ num1);	
+		}
+		else{
+			System.out.println("EL numero mayor es "+ num2);
+		}
+	}
+}
