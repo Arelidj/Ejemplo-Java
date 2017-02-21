45f87c3b8e29a6e724182e4fd6bddccd42a5ba04
AreliDJ <162t0302@itsm.edu.mx>
2017-02-20 21:16:38 -0600

    Create ejercicio4.java

---
+++ ejercicio4.java
@@ -0,0 +1,15 @@
+public class ejercicio4 {
+	public static void main(String[]args){
+		numayor();
+	}
+	public static void numayor(){
+		int num1=3;
+		int num2=5;
+		if (num1>num2){
+			System.out.println("El numero mayor es "+ num1);	
+		}
+		else{
+			System.out.println("EL numero mayor es "+ num2);
+		}
+	}
+}
