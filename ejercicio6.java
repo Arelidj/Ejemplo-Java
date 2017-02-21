335f3fc4b38fc539c7e8cc1717f6d4db43169f19
AreliDJ <162t0302@itsm.edu.mx>
2017-02-20 21:18:00 -0600

    Create ejercicio6.java

---
+++ ejercicio6.java
@@ -0,0 +1,9 @@
+public class ejercicio6 {
+	public static void main(String []args){
+		cuadrado(4);
+	}
+	public static void cuadrado(int lado){
+		int area=lado*lado;
+		System.out.println("El area del cuadrado es = "+area);
+	}
+}
