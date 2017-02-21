77d77dfa8c3667d8860b6d299ac2714ff4a2ec80
AreliDJ <162t0302@itsm.edu.mx>
2017-02-20 21:18:40 -0600

    Create ejercicio7.java

---
+++ ejercicio7.java
@@ -0,0 +1,10 @@
+public class ejercicio7 {
+	public static void main(String[]args){
+		circulo(3.141592654,5);
+	}
+	public static void circulo(double pi,double radio){
+		double area;
+		area=pi*(radio*radio);
+		System.out.println("El area del circulo es = "+ area);
+	}
+}
